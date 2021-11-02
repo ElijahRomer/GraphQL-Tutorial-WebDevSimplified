9f3c89e81476b2c73a602e82e52409439707a91e
Elijah Romer <romere88@gmail.com>
2021-11-02 18:34:53 -0500

    adjusted graphql require statement and middleware function call. Added graphql interfact to the app.listen console log.

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require(`express`);
+const { graphqlHTTP } = require('express-graphql');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`App is listening at http://localhost:${PORT}`));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    graphiql: true,
+  })
+);
+
+app.listen(
+  PORT,
+  console.log(
+    `App is listening at http://localhost:${PORT} \nView GraphQL Interface at http://localhost:3000/graphql`
+  )
+);
